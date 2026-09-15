06b6303edc8566fb35694c89379d55c61c417b2c
jinx-pravolensky <marcowpeople21@gmail.com>
2026-09-15 13:10:06 +0700

    update code

--- lib/service/api_config.dart
+++ lib/service/api_config.dart
@@ -1,21 +1,19 @@
 import 'package:flutter/foundation.dart';
 
 class ApiConfig {
-  static const String ipAddress = '192.168.1.9';
-  static String get baseUrl {
-    if (kIsWeb) {
-      return 'http://localhost:3000/api';
-    }
-    return 'http://$ipAddress:3000/api';
-  }
+  // static const String ipAddress = '192.168.1.9';
+  // static String get baseUrl {
+  //   if (kIsWeb) {
+  //     return 'http://localhost:3000/api';
+  //   }
+  //   return 'http://$ipAddress:3000/api';
+  // }
 
-  /* 
-  static const String vpsIpAddress = 'http://76.13.17.73:3000';
+  static const String vpsIpAddress = 'http://109.110.188.178:3000';
   static String get baseUrl {
     if (kIsWeb) {
       return 'http://localhost:3000/api';
     }
     return '$vpsIpAddress/api';
   }
-  */
 }
